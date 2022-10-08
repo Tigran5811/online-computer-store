7608b6376de73267982628265f04c7a3d99bfafe
gosh888 <goshhayrapetyan@gmail.com>
2022-10-08 14:34:28 +0400

    http request without express

--- add-delete-user.js
+++ add-delete-user.js
@@ -23,8 +23,34 @@ export const deleteUserName = async (login) => {
     return writeFile(usersPath, users);
 };
 
+export const getUSers = async () => {
+    const users = await readFile(usersPath);
+    return users;
+};
+
 export const deleteUserIndex = async (index) => {
     const users = await readFile(usersPath);
     users.splice(index, 1);
     return writeFile(usersPath, users);
+};
+
+const getRequestData = async (req) => new Promise((res) => {
+    req.on('data', (userData) => {
+        res(JSON.parse(userData));
+    });
+});
+
+export const userMethods = async (method, req) => {
+    switch (method) {
+        case 'GET':
+            return getUSers();
+
+        case 'POST':
+                const { username, password } = await getRequestData(req);
+                await addUser(username, password);
+                return { message: 'User created' };
+
+        default:
+            return { message: 'Method not found' };
+    }
 };
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-import { addUser, deleteUserName, deleteUserIndex } from './add-delete-user.js';
+import http from 'http';
+import { userMethods } from './add-delete-user.js';
 
-(async () => {
-    await addUser('Daniel', '1111');
-    await deleteUserName('Daniel');
-    await deleteUserIndex(0);
-}
-)();
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  switch (url) {
+    case '/user':
+      const data = await userMethods(method, req);
+      res.end(JSON.stringify(data));
+      break;
+
+    default:
+      res.end(JSON.stringify({ message: 'unknown url' }));
+      break;
+  }
+});
+
+server.listen(3000, () => {
+  console.log('server listen on localhost:3000');
+});
